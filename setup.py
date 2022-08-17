1aee12e5f36ed731cfc1b479ed5367b258a168cd
Sebastian Ruiz <sruiz@mailbox.org>
2022-08-17 13:10:54 +0200

    made install pip installable python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='context_action_framework',
+    version='0.0.1',
+    description='Context Action Framework for the ReconCycle Project',
+    license='MIT',
+    packages=['context_action_framework'],
+    author='Sebastian Ruiz and Mihael Simonič',
+    keywords=['ReconCycle'],
+    url='https://github.com/ReconCycle/context_action_framework'
+)
