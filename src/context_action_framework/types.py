7303979fd223ccef5cf946b3f86b2f14d74df28f
Sebastian Ruiz <sruiz@mailbox.org>
2022-08-30 16:39:05 +0200

    small improvements

--- src/context_action_framework/types.py
+++ src/context_action_framework/types.py
@@ -59,25 +59,30 @@ class Camera(IntEnum):
     basler = 0
     realsense = 1
 
-
-# this already exists in ROS!
-# @dataclass
-# class Transform:
-#     position: Optional[np.ndarray] = None
-#     rotation: Optional[np.ndarray] = None
-
 @dataclass
 class Location:
     module: Optional[Module] = None
     tf: Optional[Transform] = None
 
+# shorthand function
+def _trans(vect, quat):
+    return Transform(Vector3(*vect), Quaternion(*quat))
+
 @dataclass
 class Locations:
-    vision_above_vice = Location(Module.vice, Transform([0.3, 0.3, 0.4], [0.0, 0.0, -1.0, 0.0]))
-    vision_above_vice_module = Location(Module.vice, Transform([0.3, 0.3, 0.6], [0.0, 0.0, -1.0, 0.0]))
-    push_device_pin_start = Location(Module.vice, Transform([0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0])) # todo
-    push_device_pin_end = Location(Module.vice, Transform([0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0])) # todo
-    move_vice_slide = Location(Module.vice, Transform([0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0])) # todo
+    # realsense locations for looking at vice and at the whole vice module
+    vision_above_vice = Location(Module.vice, _trans([0.3, 0.3, 0.4], [0.0, 0.0, -1.0, 0.0]))
+    vision_above_vice_module = Location(Module.vice, _trans([0.3, 0.3, 0.6], [0.0, 0.0, -1.0, 0.0]))
+
+    # for pushing out the pin
+    push_device_pin_start = Location(Module.vice, _trans([0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0])) # todo
+    push_device_pin_end = Location(Module.vice, _trans([0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0])) # todo
+
+    # tf of where softhand can drop object into vice slide. Needed for moveblock
+    move_vice_slide = Location(Module.vice, _trans([0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0])) # todo
+
+    # entrance of the cutter
+    cutter_entrance = Location(Module.cutter, _trans([0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0])) # todo
 
 
 
@@ -90,12 +95,12 @@ class Detection:
     label: Optional[Label] = None
     score: Optional[float] = None
 
-    tf_px: Optional[np.ndarray] = None
+    tf_px: Optional[Transform] = None
     box_px: Optional[np.ndarray] = None
     obb_px: Optional[np.ndarray] = None
     obb_3d_px: Optional[np.ndarray] = None
 
-    tf: Optional[np.ndarray] = None
+    tf: Optional[Transform] = None
     box: Optional[np.ndarray] = None
     obb: Optional[np.ndarray] = None
     obb_3d: Optional[np.ndarray] = None
@@ -103,6 +108,8 @@ class Detection:
     polygon_px: Optional[Polygon] = None
 
     # stuff that we only use in vision internally
+    center_px: Optional[np.ndarray] = None
+    center: Optional[np.ndarray] = None
     mask: Optional[Tensor] = None
     mask_contour: Optional[np.ndarray] = None
     tracking_score: Optional[float] = None
@@ -110,22 +117,33 @@ class Detection:
 
 
 @dataclass
-class LeverAction:
+class Gap:
     id : Optional[int] = None
-    from_px: Optional[np.ndarray] = None
-    to_px: Optional[np.ndarray] = None
+
+    from_tf: Optional[Transform] = None
+    to_tf: Optional[Transform] = None
 
     from_depth: Optional[float] = None
     to_depth: Optional[float] = None
 
-    # in camera coords
-    from_camera: Optional[np.ndarray] = None
-    to_camera: Optional[np.ndarray] = None
+    from_det: Optional[Detection] = None
+    to_det: Optional[Detection] = None
+    
+    obb: Optional[np.ndarray] = None
+    obb_3d: Optional[np.ndarray] = None
+    
+    # internal stuff
+    from_px: Optional[np.ndarray] = None
+    to_px: Optional[np.ndarray] = None
 
-    obb_px: Optional[np.ndarray] = None
-    bb_camera: Optional[np.ndarray] = None
+    # # in camera coords
+    # from_camera: Optional[np.ndarray] = None
+    # to_camera: Optional[np.ndarray] = None
 
-    pose_stamped: Optional[PoseStamped] = None
+    # obb_px: Optional[np.ndarray] = None
+    # bb_camera: Optional[np.ndarray] = None
+
+    # pose_stamped: Optional[PoseStamped] = None
 
 
 def detections_to_ros(detections):
@@ -147,12 +165,14 @@ def detections_to_ros(detections):
             label = detection.label.value, 
             score = detection.score,
             
-            tf_px = Transform(Vector3(*detection.tf_px[0], 0), Quaternion(*detection.tf_px[1])),
+            tf_px = detection.tf_px,
+            # tf_px = Transform(Vector3(*detection.tf_px[0], 0), Quaternion(*detection.tf_px[1])),
             box_px = detection.box_px.astype(float).ravel().tolist(),
             obb_px = detection.obb_px.astype(float).ravel().tolist(),
             # todo: obb_3d_px = ,
 
-            tf = Transform(Vector3(*detection.tf_px[0], 0), Quaternion(*detection.tf_px[1])),
+            tf = detection.tf,
+            # tf = Transform(Vector3(*detection.tf_px[0], 0), Quaternion(*detection.tf_px[1])),
             box = detection.box.ravel().tolist(),
             obb = detection.obb.ravel().tolist(),
             # todo: obb_3d = ,
@@ -174,12 +194,14 @@ def detections_to_py(ros_detections):
             label = Label(ros_detection.label),
             score = ros_detection.score,
 
-            tf_px = [ros_detection.tf_px.translation, ros_detection.tf_px.rotation],
+            tf_px = ros_detection.tf_px,
+            # tf_px = [ros_detection.tf_px.translation, ros_detection.tf_px.rotation],
             box_px = np.asarray(ros_detection.box_px).reshape(-1, 2),
             obb_px = np.asarray(ros_detection.obb_px).reshape(-1, 2),
             # todo: obb_3d_px = ,
             
-            tf = [ros_detection.tf.translation, ros_detection.tf.rotation],
+            tf = ros_detection.tf,
+            # tf = [ros_detection.tf.translation, ros_detection.tf.rotation],
             box = np.asarray(ros_detection.box).reshape(-1, 2),
             obb = np.asarray(ros_detection.obb).reshape(-1, 2),
             # todo: obb_3d = ,
@@ -193,16 +215,27 @@ def detections_to_py(ros_detections):
 def gaps_to_ros(gaps):
     ros_gaps = []
     for gap in gaps:
-        # todo: finish this
         ros_gap = ROSGap(
             id = gap.id,
-            from_depth = gap.from_depth,
-            to_depth = gap.to_depth
+            from_tf=gap.from_tf,
+            to_tf=gap.to_tf,
+            obb=gap.obb.ravel().tolist(),
+            # todo: obb_3d=gap.obb_3d
         )
     
         ros_gaps.append(ros_gap)
     
     return ros_gaps
 
-def gaps_to_py(gaps):
-    pass
+def gaps_to_py(ros_gaps):
+    gaps = []
+    for ros_gap in ros_gaps:
+        gap = Gap(
+            id=ros_gap.id,
+            from_tf=ros_gap.from_tf,
+            to_tf=ros_gap.to_tf,
+            obb=np.asarray(ros_gap.obb).reshape(-1, 2)
+            # todo: obb_3d=ros_gap.obb_3d
+        )
+        gaps.append(gap)
+    return gaps
